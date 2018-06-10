bd365764e5bb4f7b2f0d59ce254297b0949a8a29
ChouJiaChen <1426257066@qq.com>
2018-06-10 20:00:32 +0800

    添加项目文件。

---
+++ MeasurementLevel/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace MeasurementLevel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtView = new System.Windows.Forms.DataGridView();
+            this.Input = new System.Windows.Forms.Button();
+            this.pointNum = new System.Windows.Forms.TextBox();
+            this.Cal = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtView
+            // 
+            this.dtView.BackgroundColor = System.Drawing.Color.GhostWhite;
+            this.dtView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtView.Location = new System.Drawing.Point(169, 47);
+            this.dtView.Name = "dtView";
+            this.dtView.RowTemplate.Height = 27;
+            this.dtView.Size = new System.Drawing.Size(1063, 384);
+            this.dtView.TabIndex = 0;
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(26, 139);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(122, 36);
+            this.Input.TabIndex = 1;
+            this.Input.Text = "输入未知点";
+            this.Input.UseVisualStyleBackColor = true;
+            this.Input.Click += new System.EventHandler(this.Input_Click);
+            // 
+            // pointNum
+            // 
+            this.pointNum.Location = new System.Drawing.Point(41, 85);
+            this.pointNum.Name = "pointNum";
+            this.pointNum.Size = new System.Drawing.Size(75, 25);
+            this.pointNum.TabIndex = 2;
+            // 
+            // Cal
+            // 
+            this.Cal.Location = new System.Drawing.Point(26, 196);
+            this.Cal.Name = "Cal";
+            this.Cal.Size = new System.Drawing.Size(122, 34);
+            this.Cal.TabIndex = 3;
+            this.Cal.Text = "计算";
+            this.Cal.UseVisualStyleBackColor = true;
+            this.Cal.Click += new System.EventHandler(this.Cal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1295, 486);
+            this.Controls.Add(this.Cal);
+            this.Controls.Add(this.pointNum);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.dtView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtView;
+        private System.Windows.Forms.Button Input;
+        private System.Windows.Forms.TextBox pointNum;
+        private System.Windows.Forms.Button Cal;
+    }
+}
+
---
+++ MeasurementLevel/Form1.cs
@@ -0,0 +1,103 @@
+﻿/***************************************************************
+ * @Author:Ahmiao
+ * @date:2018/6/10
+ * Describe:
+ * A simple leveling measurement promgram
+ *In the surveying and mapping,
+ * we ofen use level data to caculation.
+ * in this promgram,I use table to storage  data
+ *  
+ ****************************************************************/
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MeasurementLevel
+{
+    public partial class Form1 : Form
+    {
+        public int id = 0;//input the unkown poit numble as global variable
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// table
+        /// </summary>
+        /// <returns></returns>
+        private DataTable ShowTable()
+        {
+            //init table
+            DataTable mydt = new DataTable();
+            mydt.Columns.Add("id", Type.GetType("System.String"));
+            mydt.Columns.Add("点号", Type.GetType("System.String"));
+            mydt.Columns.Add("距离（km）", Type.GetType("System.String"));
+            mydt.Columns.Add("测得高差（m）", Type.GetType("System.String"));
+            mydt.Columns.Add("高差改正（mm）", Type.GetType("System.String"));
+            mydt.Columns.Add("改正后高差（m）", Type.GetType("System.String"));
+            mydt.Columns.Add("高程（m）", Type.GetType("System.String"));
+            for (int myr = 0; myr <= id+1 ; myr++)
+            {
+                mydt.Rows.Add(myr + 1);
+            }
+            return mydt;
+
+        }
+        /// <summary>
+        /// input the numble and display the table
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Input_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                id = Convert.ToInt16(pointNum.Text);
+                if (id <= 0)
+                    return;//if point num <=0,Jump out function
+                dtView.DataSource = ShowTable();
+                dtView.Rows[0].Cells[6].Style.BackColor = Color.LightCyan;
+                dtView.Rows[id+1].Cells[6].Style.BackColor = Color.LightCyan;
+                for(int i=1;i<=id+1;i++)
+               {
+                    dtView.Rows[i].Cells[2].Style.BackColor = Color.LightCyan;
+                    dtView.Rows[i].Cells[3].Style.BackColor = Color.LightCyan;
+                    dtView.Rows[i].Cells[4].Style.BackColor = Color.DeepPink;
+                    dtView.Rows[i].Cells[5].Style.BackColor = Color.DeepPink;
+                }
+                for (int i = 0; i <= id + 1; i++)
+                {
+                    dtView.Rows[i].Cells[1].Style.BackColor = Color.LightCyan;
+                }
+
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("请输入正确的未知点个数");
+            }
+           
+        }
+        /// <summary>
+        /// caculate data
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Cal_Click(object sender, EventArgs e)
+        {
+            double Ha, Hb;
+            Ha = Convert.ToDouble(dtView.Rows[0].Cells[6].Value);//Starting elevation
+            Hb = Convert.ToDouble(dtView.Rows[id].Cells[6].Value);//Ending elevation
+            double[] dis = new double[id+1];
+
+        }
+    }
+}
